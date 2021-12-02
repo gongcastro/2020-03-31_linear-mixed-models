4da8c2224983f51bdf4515283cd1ea948adf7da4
Gonzalo García-Castro <31894043+gongcastro@users.noreply.github.com>
2021-12-02 23:27:09 +0100

    Reduce gif size

--- R/plots.R
+++ R/plots.R
@@ -53,7 +53,7 @@ ggplot(dat, aes(frequency, rt)) +
   stat_summary(aes(group = 1), fun = "mean", geom = "line", size = 1) +
   geom_text(
     x = 1.5, y = 2, 
-    label = paste0("Intercept: ", round(model$coefficients[1], 2), "\nSlope: ", round(model$coefficients[2], 2))
+    label = paste0("Intercept: ", round(fit_all$coefficients[1], 2), "\nSlope: ", round(fit_all$coefficients[2], 2))
   ) +
   labs(x = "Frequency", y = "Reaction time (s)", colour = "Participant", subtitle = "No random effects") +
   scale_colour_manual(values = wes_palette("Darjeeling1", 5, type = "discrete"))
@@ -102,7 +102,7 @@ ggplot(dat, aes(frequency, rt, colour = participant)) +
     x = 1.5, y = 3, colour = "black", geom = "label",
     label = paste0("Intercept: ", round(fixef(fit_int)[1], 2), "\nCoefficient: ", round(fixef(fit_int)[2], 2)),
   ) +
-  geom_text(data = model_int_coefs, aes(x = x1, y = y1, label = paste0("Intercept: ", intercept, "\nSlope: ", slope)),
+  geom_text(data = fit_int_coefs, aes(x = x1, y = y1, label = paste0("Intercept: ", intercept, "\nSlope: ", slope)),
             colour = "black", show.legend = FALSE, size = 3) +
   labs(x = "Frequency", y = "Reaction time (s)", colour = "Participant",
        subtitle = "Random intercepts by participant") +
@@ -114,7 +114,7 @@ ggsave(here("img", "plot_model_int.png"))
 # visualise predictions of model with random intercepts and slopes ----
 
 # get model coefficients
-model_slo_coefs <- data.frame(
+fit_slo_coefs <- data.frame(
   participant = paste0("participant", 1:5),
   x1 = 2.3,
   y1 = seq(1, 3.5, length.out = 5),
@@ -124,15 +124,15 @@ model_slo_coefs <- data.frame(
 
 ggplot(dat, aes(frequency, rt, colour = participant)) +
   geom_jitter(alpha = 0.8, size = 3, width = 0.1) +
-  stat_summary(aes(y = fitted(model_slo), group = participant), fun = "mean", geom = "point", size = 4) +
-  stat_summary(aes(y = fitted(model_slo), group = participant), fun = "mean", geom = "line", size = 1) +
-  stat_summary(aes(y = predict(model_int), group = 1), fun.data = "mean_se", geom = "ribbon", colour = NA, alpha = 0.5) +
+  stat_summary(aes(y = fitted(fit_slo), group = participant), fun = "mean", geom = "point", size = 4) +
+  stat_summary(aes(y = fitted(fit_slo), group = participant), fun = "mean", geom = "line", size = 1) +
+  stat_summary(aes(y = predict(fit_slo), group = 1), fun.data = "mean_se", geom = "ribbon", colour = NA, alpha = 0.5) +
   
-  stat_summary(aes(y = fitted(model_slo), group = 1), fun = "mean", geom = "point", size = 4) +
-  stat_summary(aes(y = fitted(model_slo), group = 1), fun = "mean", geom = "line", size = 1) +
+  stat_summary(aes(y = fitted(fit_slo), group = 1), fun = "mean", geom = "point", size = 4) +
+  stat_summary(aes(y = fitted(fit_slo), group = 1), fun = "mean", geom = "line", size = 1) +
   annotate(
     x = 1.5, y = 3, colour = "black", geom = "label",
-    label = paste0("Intercept: ", round(fixef(model_slo)[1], 2), "\nCoefficient: ", round(fixef(model_slo)[1], 2))) +
+    label = paste0("Intercept: ", round(fixef(fit_slo)[1], 2), "\nCoefficient: ", round(fixef(fit_slo)[1], 2))) +
   geom_text(
     data = model_slo_coefs,
     aes(x = x1, y = y1, label = paste0("Intercept: ", intercept, "\nSlope: ", slope)), 
@@ -180,7 +180,7 @@ predictions_animate <- ggplot(predictions, aes(x = frequency, y = rt)) +
   transition_states(states = model, transition_length = 3, state_length = 2, wrap = TRUE)
 
 animate(
-  predictions_animate, height = 2000, width = 2000, 
+  predictions_animate, height = 500, width = 500, 
   fps = 30, rewind = TRUE, duration = 10, device = "png"
 )
 
